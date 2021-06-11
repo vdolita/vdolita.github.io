82a1ed2794c403fa6a9a3cbfa8b149dff0e2c117
Vdolita <vdolita@outlook.com>
2021-06-11 17:08:50 +0800

    fix sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,7 +9,7 @@ module.exports = {
     'gatsby-plugin-styled-components',
     'gatsby-plugin-image',
     'gatsby-plugin-react-helmet',
-    "gatsby-plugin-sitemap",
+    // "gatsby-plugin-sitemap",
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
