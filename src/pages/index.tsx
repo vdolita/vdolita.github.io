a66e8033cb5c0a4c8b73ce4da50a41d911658285
Vdolita <vdolita@outlook.com>
2021-06-20 23:20:04 +0800

    dev

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,29 +1,33 @@
-import { Tabs } from 'antd';
 import React from 'react';
 import Layout from '../components/layout';
 import Seo from '../components/seo';
 
-const { TabPane } = Tabs;
+const LinkStyle: React.CSSProperties = {
+  fontSize: '40px',
+  color: '#fff',
+  margin: '0 100px',
+};
+
 const IndexPage = () => (
-  <Layout isShowHeader>
+  <Layout>
     <Seo title="vdolita" />
-    <div style={{ margin: '10px 0 0 110px' }}>
-      <Tabs
-        defaultActiveKey="1"
-        tabPosition="left"
-        tabBarStyle={{ color: '#fff' }}
-        type="line"
-      >
-        <TabPane tab="杂谈" key="1">
-          Content of Tab Pane 1
-        </TabPane>
-        <TabPane tab="小说" key="2">
-          Content of Tab Pane 2
-        </TabPane>
-        <TabPane tab="奇物" key="3">
-          Content of Tab Pane 3
-        </TabPane>
-      </Tabs>
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <a style={LinkStyle} href="/artical">
+        杂谈
+      </a>
+      <a style={LinkStyle} href="/novel">
+        小说
+      </a>
+      <a style={LinkStyle} href="/artical">
+        奇物
+      </a>
     </div>
   </Layout>
 );
